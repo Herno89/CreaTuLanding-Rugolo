6be4ec60552b31f84e1a15c5f23cf47e2173ab6a
Herno <114448348+Herno89@users.noreply.github.com>
2024-08-27 15:43:54 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import ItemCount from './Components/ItemCount'
-import NavB from './Components/NavB'
+import NavBar from './Components/NavBar'
 
 function App() {
   
 
   return (
     <div>
-      <NavB/>
+      <NavBar/>
       <ItemListContainer greeting={"Bienvenidos"}/>
       <ItemCount/>
     </div>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,33 @@
 import CartWidget from "./CartWidget/CartWidget"
+
+const NavBar =()=>{
+    return (
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">Celulares</a>
+        <a className="nav-link" href="#">Tablets</a>
+        <a className="nav-link" href="#">Notebooks</a>
+        <a className="nav-link disabled" aria-disabled="true">Proximamente</a>
+      </div>
+    </div>
+  </div>
+  <CartWidget />
+</nav>
+
+
+    )
+}
+export default NavBar
+
+
+/*
 const NavBar =()=>{
     return (
 <nav>
@@ -13,3 +42,5 @@ const NavBar =()=>{
     )
 }
 export default NavBar
+*/
+
